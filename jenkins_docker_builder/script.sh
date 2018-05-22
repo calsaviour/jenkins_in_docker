0da6c06a0540908a6474d6f9dc9b4e4b63f5737c
Calvin Low <calsaviour@gmail.com>
2018-05-22 08:55:57 +0000

    Add step to install wget, remove step to start jenkins as this is to buidl the image only

--- jenkins_docker_builder/script.sh
+++ jenkins_docker_builder/script.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "Staring installation of Java 1.8"
-sudo yum install java-1.8.0-openjdk.x86_64 -y
+yum install java-1.8.0-openjdk.x86_64 -y
 echo "Complete installation of Java 1.8"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "Staring installation of Jenkins"
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins -y
+echo "Starting installation of Jenkins"
+yum install wget -y
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins -y
 #sudo firewall-cmd –zone=public –add-port=8080/tcp --permanent
 #sudo firewall-cmd –zone=public –add-service=http --permanent
 echo "Complete installation of Jenkins"
