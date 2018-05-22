27c3d2fe8857fbe137f0a2530e4c4aaa99cbdbc8
calsaviour <calsaviour@gmail.com>
2018-05-22 16:31:14 +0800

    Remove the firewall step in script

--- jenkins_docker_builder/script.sh
+++ jenkins_docker_builder/script.sh
@@ -11,8 +11,8 @@ echo "Staring installation of Jenkins"
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins -y
-sudo firewall-cmd –zone=public –add-port=8080/tcp --permanent
-sudo firewall-cmd –zone=public –add-service=http --permanent
+#sudo firewall-cmd –zone=public –add-port=8080/tcp --permanent
+#sudo firewall-cmd –zone=public –add-service=http --permanent
 echo "Complete installation of Jenkins"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
